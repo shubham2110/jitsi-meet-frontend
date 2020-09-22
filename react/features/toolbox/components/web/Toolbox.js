8fa49006f9160a79e015d2dec04c66ca4c88f01b
Sanju <sanjuashish1993@gmail.com>
2020-09-22 10:14:57 +0530

    s

--- react/features/toolbox/components/web/Toolbox.js
+++ react/features/toolbox/components/web/Toolbox.js
@@ -34,6 +34,7 @@ import { connect, equals } from '../../../base/redux';
 import { OverflowMenuItem } from '../../../base/toolbox/components';
 import { getLocalVideoTrack, toggleScreensharing } from '../../../base/tracks';
 import { VideoBlurButton } from '../../../blur';
+import { UploadBackGroundImage } from '../../../blur';
 import { CHAT_SIZE, ChatCounter, toggleChat } from '../../../chat';
 import { EmbedMeetingDialog } from '../../../embed-meeting';
 import { SharedDocumentButton } from '../../../etherpad';
@@ -1031,7 +1032,12 @@ class Toolbox extends Component<Props, State> {
             <VideoBlurButton
                 key = 'videobackgroundblur'
                 showLabel = { true }
-                visible = { this._shouldShowButton('videobackgroundblur') && !_screensharing } />,
+                visible={this._shouldShowButton('videobackgroundblur') && !_screensharing} />,
+            <UploadBackGroundImage 
+                key='backgroundImage'
+                Label='Background Image'
+                visible={this._shouldShowButton('videobackgroundblur') && !_screensharing}
+            />,
             <SettingsButton
                 key = 'settings'
                 showLabel = { true }
