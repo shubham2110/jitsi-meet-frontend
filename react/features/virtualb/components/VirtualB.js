42c8f69bdab4dd841217d14f1ac2fe3316aa1329
Sanju <sanjuashish1993@gmail.com>
2020-09-30 10:22:49 +0530

    Added New

--- react/features/virtualb/components/VirtualB.js
+++ react/features/virtualb/components/VirtualB.js
@@ -1,46 +1,49 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 
 class VirtualB extends Component {
-    state = { 
-  
+    state = {
+
         // Initially, no file is selected 
         selectedFile: null
-      }; 
-       
-      // On file select (from the pop up) 
-      onFileChange = event => { 
-       
+    };
+
+    componentDidMount() {
+        this.setState({
+            selectedFile = localStorage.getItem("backgroundImage");
+        })
+    }
+
+    // On file select (from the pop up) 
+    onFileChange = event => {
+
         // Update the state 
         this.setState({
             selectedFile: URL.createObjectURL(event.target.files[0])
-          });
-        
+        });
 
-       
-      }; 
-       
-      // On file upload (click the upload button) 
-      onFileUpload = () => { 
-       
-        // Details of the uploaded file 
-        localStorage.setItem("backgroundImage", this.state.selectedFile);
-        console.log(this.state.selectedFile); 
-      }; 
-       
-    render() { 
-        return (
-            <div>
-                <input type="file" onChange={this.onFileChange} /> 
-                <button onClick={this.onFileUpload}> 
-                  Upload! 
-                </button> 
-                <img src={this.state.selectedFile}/>
 
-                </div>
-        )
-    }
-}
- 
+
+    };
+
+    // On file upload (click the upload button) 
+    deleteImage = () => {
+        localStorage.setItem('backgroundImage', null);
+    };
+
+    render() {
+      return (
+        <React.Fragment> {
+                    (!this.state.selectedFile ? <input value="Virtual Background" type="file"
+                        onChange = {
+                            this.onFileChange
+                        }
+                        /> : <button onClick={this.deleteImage}>
+                        Clear </button>)}  
+          </React.Fragment>
+                    )
+                }
+  }
+
 export default VirtualB;
-
-
