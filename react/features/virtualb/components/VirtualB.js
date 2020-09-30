0673adf66b6d5f84ce56f8d185d059e72b7abb2c
Sanju <sanjuashish1993@gmail.com>
2020-09-30 11:11:35 +0530

    Added New

--- react/features/virtualb/components/VirtualB.js
+++ react/features/virtualb/components/VirtualB.js
@@ -22,8 +22,8 @@ class VirtualB extends Component {
     canvas.width = img.width;
     canvas.height = img.height;
 
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
+    // var ctx = canvas.getContext("2d");
+    // ctx.drawImage(img, 0, 0);
 
     var dataURL = canvas.toDataURL("image/png");
 
